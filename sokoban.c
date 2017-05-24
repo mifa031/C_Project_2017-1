46c14502639bcbd9d660f9d3145ad007d2ddd464
mifa031 <scifimov@naver.com>
2017-05-24 16:45:09 +0900

    Add files via upload

--- sokoban.c
+++ sokoban.c
@@ -1,33 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
 #define MAX_STAGE 5
 
-char map_data[MAX_STAGE][][];
-char map[][];
+char** map;
+int map_rows;
+int map_cols;
+
+void readMap(int level);
+void printMap();
+
+void main(){
+    readMap(1);
+}
 
 void readMap(int level){
     FILE* map_file;
     int map_level = 0;
-    char object;
+    int row=0, col=0;
+    char map;
+    int temp_col1 = 0;
+    int temp_col2 = 0;
 
     map_file = fopen("map.txt","r");
-    while(object = fgets(map_file) != EOF){
-        if(object == 'm'){
+    while((map = fgetc(map_file)) != EOF){
+        temp_col1++;
+
+        if(map == 'm'){
             map_level++;
-            if(map_level > level)
+            if(map_level > level){
+                map_level = level;
                 break;
+            }
         }
-        if(object == '\n'){
-            map
+        if(map == '\n'){
+            row++;
+            if(temp_col1 > temp_col2){
+                temp_col2 = temp_col1;
+                temp_col1 = 0;
+            }
         }
-
-        printf("%c",object);
-        fscanf(map_file,"%c",&object)
-
     }
+    map_rows = row-1;
+    map_cols = temp_col2;
 
+    printf("%d\n",map_rows);
+    printf("%d\n%d\n",map_cols,map_level);
 }
 
-void main(){
-    while(1){
-        readMap();
+void printMap(){
+    for(int i=0; i<map_rows; i++){
+       for(int j=0; j<map_cols; j++)
+            printf("%c",map[i][j]);
     }
 }
